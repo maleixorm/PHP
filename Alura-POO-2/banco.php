7c4668fffaae4f9d4654b0ddcd19a67ecd3bbbc8
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-07-16 16:22:57 -0300

    Adicionando as classes Endereço e Funcionário no Projeto

--- Alura-POO-2/banco.php
+++ Alura-POO-2/banco.php
@@ -1,22 +1,27 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Endereco.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+$endereco = new Endereco('Cidade', 'Bairro', 'Minha Rua', '71A');
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
 $primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
 
-echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
-echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
-echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+echo $primeiraConta->recuperaNomeTitular() . '<br>';
+echo $primeiraConta->recuperaCpfTitular() . '<br>';
+echo $primeiraConta->recuperaSaldo() . '<br>';
 
-$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
 $segundaConta = new Conta($patricia);
 var_dump($segundaConta);
+echo '<br>';
 
-$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg'));
+$outroEndereco = new Endereco('Cidade Nova', 'Novo', 'Rua Nova', '71B');
+$abignail = new Titular(new CPF('123.654.789-01'), 'Abignail', $outroEndereco);
+$outra = new Conta($abignail);
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
--- Alura-POO-2/src/Titular.php
+++ Alura-POO-2/src/Titular.php
@@ -2,14 +2,16 @@
 
 class Titular
 {
-    private $cpf;
-    private $nome;
+    private CPF $cpf;
+    private string $nome;
+    private Endereco $endereco;
 
-    public function __construct(CPF $cpf, string $nome)
+    public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
         $this->cpf = $cpf;
         $this->validaNomeTitular($nome);
         $this->nome = $nome;
+        $this->endereco = $endereco;
     }
 
     public function recuperaCpf(): string
@@ -28,5 +30,10 @@ class Titular
             echo "Nome precisa ter pelo menos 5 caracteres";
             exit();
         }
+    }
+
+    public function recuperaEndereco() : Endereco
+    {
+        return $this->endereco;
     }
 }
