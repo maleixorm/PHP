03af11a8e022e7dd9f96ba090c5f6b44f622c441
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-07-21 16:18:15 -0300

    Interfaces no PHP

--- Alura-POO-2/bonificacoes.php
+++ Alura-POO-2/bonificacoes.php
@@ -4,32 +4,36 @@ require_once './autoload.php';
 
 use Alura\Banco\Service\ControladorDeBonificacoes;
 use Alura\Banco\Modelo\CPF;
-use Alura\Banco\Modelo\Funcionario\{Desenvolvedor, Gerente, Diretor};
+use Alura\Banco\Modelo\Funcionario\{Desenvolvedor, Gerente, Diretor, EditorVideo};
 
 $umFuncionario = new Desenvolvedor(
     'Vinicius Dias',
     new CPF('123.456.789-10'),
-    'Desenvolvedor',
     1000
 );
 
 $umaFuncionaria = new Gerente(
     'Patrícia',
     new CPF('987.654.321-10'),
-    'Gerente',
     3000
 );
 
 $umDiretor = new Diretor(
     'Ana',
     new CPF('987.654.321-12'),
-    'Diretor',
     5000
+);
+
+$umEditor = new EditorVideo(
+    'Paulo',
+    new CPF('456.987.231-10'),
+    1500
 );
 
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncionario);
 $controlador->adicionaBonificacaoDe($umaFuncionaria);
 $controlador->adicionaBonificacaoDe($umDiretor);
+$controlador->adicionaBonificacaoDe($umEditor);
 
 echo $controlador->recuperaTotal();
--- Alura-POO-2/src/Modelo/Funcionario/Funcionario.php
+++ Alura-POO-2/src/Modelo/Funcionario/Funcionario.php
@@ -7,19 +7,12 @@ use Alura\Banco\Modelo\CPF;
 
 abstract class Funcionario extends Pessoa
 {
-    private string $cargo;
     private float $salario;
 
-    public function __construct(string $nome, CPF $cpf, string $cargo, float $salario)
+    public function __construct(string $nome, CPF $cpf, float $salario)
     {
         parent::__construct($nome, $cpf);
-        $this->cargo = $cargo;
         $this->salario = $salario;
-    }
-
-    public function recuperaCargo() : string
-    {
-        return $this->cargo;
     }
 
     public function recuperaSalario() : float
@@ -42,8 +35,5 @@ abstract class Funcionario extends Pessoa
         $this->salario += $valorAumento;
     }
 
-    public function calculaBonificacao(): float
-    {
-        return $this->salario * 0.1;
-    }
+    abstract public function calculaBonificacao(): float;
 }
