9cec5196cedd140a6cfa5371a8aed1850d14462f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-07-22 21:18:00 -0300

    Multi catch no PHP

--- Alura-Exceptions/pilha.php
+++ Alura-Exceptions/pilha.php
@@ -1,29 +1,37 @@
 <?php
 
+function pulaLinha() {
+    echo '<br>';
+}
+
 function funcao1()
 {
-    echo 'Entrei na função 1' . PHP_EOL;
+    echo 'Entrei na função 1' . pulaLinha();
     try {
-        $arrayFixo = new SplFixedArray(2);
-        $arrayFixo[3] = 'Valor';
-        // $divisao = intdiv(5,0);
-    } catch (RuntimeException $error) {
-        echo 'Aconteceu um erro na função 1.' . PHP_EOL;
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $erro) {
+        echo $erro->getMessage() . pulaLinha();
+        echo $erro->getLine() . pulaLinha();
+        echo $erro->getTraceAsString() . pulaLinha();
     }
     
-    funcao2();
-    echo 'Saindo da função 1' . PHP_EOL;
+    echo 'Saindo da função 1' . pulaLinha();
 }
 
 function funcao2()
 {
-    echo 'Entrei na função 2' . PHP_EOL;
+    echo 'Entrei na função 2' . pulaLinha();
+
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+    $divisao = intdiv(5,0);
+
     for ($i = 1; $i <= 5; $i++) {
-        echo $i . PHP_EOL;
+        echo $i . pulaLinha();
     }
-    echo 'Saindo da função 2' . PHP_EOL;
+    echo 'Saindo da função 2' . pulaLinha();
 }
 
-echo 'Iniciando o programa principal' . PHP_EOL;
+echo 'Iniciando o programa principal' . pulaLinha();
 funcao1();
-echo 'Finalizando o programa principal' . PHP_EOL;
+echo 'Finalizando o programa principal' . pulaLinha();
