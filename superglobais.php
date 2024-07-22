39b712e11a1d0cb09bfa95d11a855bc8101c2080
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-07-22 10:51:34 -0300

    Definindo as super Globais do PHP

---
+++ superglobais.php
@@ -0,0 +1,10 @@
+<?php
+
+$_COOKIE // Para guardar informações
+$_SESSION // Para controle das informaçoes
+$_ENV // Resgatar valores de arquivos
+$_FILES // Resgatar arquivos enviados
+$_GET // Receber variáveis por parâmetro
+$_POST // Receber variáveis por referência
+$_REQUEST // Recebe variaveis das duas formas anteriores
+$_SERVER // Funções globais do site
