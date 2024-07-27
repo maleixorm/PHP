7c6b1b9dfdc2134e00bdec5afb4c13d0d321de54
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-07-27 18:43:45 -0300

    Trabalhando com variáveis do servidor

--- variaveisambiente.php
+++ variaveisambiente.php
@@ -6,3 +6,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 var_dump($_ENV['USER']);
+
+echo '<br><br>';
+
+var_dump($_SERVER);
---
+++ variaveisservidor.php
@@ -0,0 +1,9 @@
+<?php
+
+require './vendor/autoload.php';
+
+echo __DIR__;
+
+echo '<br><br>';
+
+var_dump($_SERVER);
