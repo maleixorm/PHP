b3cf9db47346fff9d3c04fd887c4c204df1d212b
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-07-29 06:11:10 -0300

    Trabalhando com arquivos enviados por formulários

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
-<?php
-
-session_start();
-
-echo session_id();
-
-require "./session.php";
-
-$_SESSION['name'] = 'Marcos';
-$_SESSION['person'] = ['name'=> 'André', 'age' => 37];
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+    <form action="teste.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="img">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
